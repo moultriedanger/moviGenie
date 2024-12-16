628bacc68f2f9be4bc3c6cdd90610f737cab034c
Matthew Keller <matthewjameskeller@outlook.com>
2024-12-16 00:17:47 -0500

    final

--- www/static/js/filter.js
+++ www/static/js/filter.js
@@ -13,7 +13,7 @@ function applyFilters() {
     });
 
     // Fetch filtered movies
-    fetch(`/movies?${params.toString()}`)
+    fetch('/moviGenie/movies?${params.toString()}')
         .then(response => response.json())
         .then(data => {
             displayMovies(data); // Function to update the UI with filtered movies
@@ -27,7 +27,7 @@ function displayMovies(movies) {
 
     movies.forEach(movie => {
         const movieCard = `
-            <div class="movie_card" onclick="window.location.href='/trending_movie/${movie.id}'">
+            <div class="movie_card" onclick="window.location.href='moviGenie/trending_movie/${movie.id}'">
                 <img src="https://image.tmdb.org/t/p/w1280${movie.poster_path}" alt="Movie Poster" />
                 <h1>${movie.title}</h1>
                 <p>Rating: ${movie.vote_average}</p>
